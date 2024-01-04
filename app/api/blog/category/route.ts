9c5d7a4907f1b5c270a9b6944b2bacdec0150274
joon-guthub <ehdfkd2000@naver.com>
2024-01-05 07:06:22 +0900

    fetch testing

--- app/api/blog/category/route.ts
+++ app/api/blog/category/route.ts
@@ -6,8 +6,23 @@ export async function GET(request: Request) {
     //OFFSET 시작번호 ROWS FETCH NEXT 페이지보여줄갯수 ROWS ONLY
     const result =
       await sql`SELECT * FROM my_blog_category;`;
-    return NextResponse.json({ result }, { status: 200 });
+    return NextResponse.json({ result }, { status: 200,
+      headers: {
+      'Cache-Control': 'no-store', // Disable caching
+    }, });
   } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+type PostRequestData = {
+  newName: string;
+};
+export async function POST(request: Request){
+  try{
+    const { newName }: PostRequestData = await request.json();
+    await sql`INSERT INTO my_blog_category (category_name) VALUES (${newName});`;
+    return NextResponse.json({ success: true }, { status: 200 });
+  }catch (error) {
     return NextResponse.json({ error }, { status: 500 });
   }
 }
--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from '@/app/main.module.scss'
 import { baseUrl } from '../utils/baseUrl';
+import Test from './Test';
 
 
 
@@ -11,18 +12,15 @@ type CategorysType = {
 
 const BlogPage = async () => {
   const categoryData = await fetch(`${baseUrl}/api/blog/category`,{ 
-    next: { revalidate: 0 },
+    next: { revalidate: 10 },
    });
   const {result:{rows:categorys}} = await categoryData.json()
   
   console.log('next',categorys)
-  // const categoryData2 = await fetch(`${baseUrl}/api/blog/category`,{ 
-  //   cache: 'no-store',
-  //  });
-  // const {result:{rows:categorys2}} = await categoryData2.json()
-  // console.log('ser',categorys2)
+
   return (
     <main className={`${styles.main}`}>
+      <Test/>
       <div>
         {categorys.map(({category_id,category_name}:CategorysType)=>{
           return(
