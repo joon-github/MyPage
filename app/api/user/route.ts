8a1b9d1a9093628233abebd898e47d8ba2dec23c
BeomJoon-MSB <ehdfkd@benter.co.kr>
2024-01-04 13:40:15 +0900

    dev test

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   try {
-    //OFFSET 시작번호 ROWS FETCH NEXT 페이지보여줄갯수 ROWS ONLY
+    //OFFSET 시작번호 ROWS FETCH NEXT 페이지보여줄갯수 ROWS ONLY 
     const result =
       await sql`SELECT * FROM users OFFSET 1 ROWS FETCH NEXT 1 ROWS ONLY;`;
     return NextResponse.json({ result }, { status: 200 });
