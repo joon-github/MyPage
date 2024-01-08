9cfd4ca779919fe82acc17905dd6d94802f1301c
joon-guthub <ehdfkd2000@naver.com>
2024-01-08 23:22:59 +0900

    blog code 작성

--- app/blog/_blogComponents/post/Post.tsx
+++ app/blog/_blogComponents/post/Post.tsx
@@ -1,21 +1,24 @@
+'use client'
 import React from 'react';
 import { PostType } from './Posts';
-
+import blogStyles from '@/app/blog/blog.module.scss'
 interface PostPropsType {
   data:PostType[];
   year:string;
 }
 
 const Post = ({data,year}:PostPropsType) => {
-  console.log('data',data)
   return (
-    <article>
-      <div>{year}</div>
+    <article className={blogStyles.postList}>
+      <h3>{year}</h3>
       {data.map((post:PostType)=>{
         return(
-          <div>
-            <div>{post.title}</div>
-            <div>{post.sub_title}</div>
+          <div key={post.post_id} className={blogStyles.postDataWrapper}>
+            <span className={blogStyles.date}>{post.create_at.substr(5,5)}</span>
+            <div>
+              <h4 className={blogStyles.postTitle}>{post.title}</h4>
+              <h5 className={blogStyles.postSubTitle}>{post.sub_title}</h5>
+            </div>
           </div>
         )
       })}
