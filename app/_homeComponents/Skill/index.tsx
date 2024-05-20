70b3650239862ea621be33f925605ebfa5d0f438
joon-guthub <ehdfkd2000@naver.com>
2024-05-20 23:23:45 +0900

    test commit

--- app/_homeComponents/Skill/index.tsx
+++ app/_homeComponents/Skill/index.tsx
@@ -40,6 +40,7 @@ const Skill = () => {
       ],
     },
   ];
+
   return (
     <section className="flex flex-col h-full flex justify-center sm:pb-[100px] pb-0 px-10 pt-10">
       <h3 className="text-2xl mb-10 text-blue-900">SKILLS</h3>
